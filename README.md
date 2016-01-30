dc1481d79cf2bbbb06f991140a9c9e8df89d256a
Navjot Ahuja <navjotahuja92@gmail.com>
2016-01-31 03:21:50 +0530

    Alarm First Commit. Done

---
+++ README.md
@@ -0,0 +1,11 @@
+## Alarm npm package
+
+This package lets you set short alarms for deadlines.
+
+## Usage 
+
+Suppose you want to finish a task within 30 minutes:
+
+You can open a terminal dialog box (Alt + F2 on Linux Mint) and run the following command
+
+`alarm -min=30 -msg="Time Over"`
---
+++ index.js
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+
+var exec = require('child_process').exec;
+
+var allowedMinutesArgs = ["min", "minutes"];
+var allowedMessageArgs = ["msg", "message"];
+
+// Get Args
+var args = process.argv.slice(2);
+var minutes, message;
+for(key in args){
+	var arg = args[key]
+	arg = arg.replace("-","");
+	input = arg.split("=");
+	if(allowedMinutesArgs.indexOf(input[0]) > -1){
+		minutes = input[1]
+	}
+
+	if(allowedMessageArgs.indexOf(input[0]) > -1){
+		message = input[1]
+	}
+}
+
+function printExampleUsage(){
+	console.log("Example Usage:");
+	console.log("alarm min=5 msg=\"Time Over\"");
+	console.log("alarm minutes=5 message=\"Time Over\"");
+}
+
+if(!minutes){
+	console.error("Error! No minutes Provided.");
+	printExampleUsage();
+	process.exit();
+}
+
+if(!message){
+	console.error("Error! No Message Provided.");
+	printExampleUsage();
+	process.exit();
+}
+
+minutes = minutes * 60;
+var command = "(sleep " + minutes + "; notify-send \"" + message + "\") &";
+
+exec(command);
